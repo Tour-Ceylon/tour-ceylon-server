3cd5e3b51c7b2bffcf171afb9e14efbfd921076b
namindu-07 <namidu1234569@gmail.com>
2026-04-09 10:20:03 +0530

    Expand CORS origins to cover local dev ports 3000-3005

--- app/config/settings.py
+++ app/config/settings.py
@@ -15,8 +15,16 @@ class Settings:
     BACKEND_CORS_ORIGINS: List[str] = [
         "http://localhost:3000",
         "http://localhost:3001",
+        "http://localhost:3002",
+        "http://localhost:3003",
+        "http://localhost:3004",
+        "http://localhost:3005",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
+        "http://127.0.0.1:3002",
+        "http://127.0.0.1:3003",
+        "http://127.0.0.1:3004",
+        "http://127.0.0.1:3005",
     ]
     
     # Clerk Authentication
